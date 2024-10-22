993db5dba7d926e7cb71b2592e5078c73ecfe22f
Chamber6821 <vania.osinin@yandex.ru>
2024-10-23 02:57:14 +0300

    Add useNativeText

--- hooks/useNativeText.ts
+++ hooks/useNativeText.ts
@@ -1,3 +1,29 @@
-export const useNativeText = () => ({
+import { useMemo, useState } from "react"
 
-})
+export type Language = 'ru' | 'en'
+
+export type AllTexts = 'welcome' | 'thisIsMyUniWork'
+
+const texts: Record<AllTexts, Record<Language, string>> = {
+  welcome: {
+    ru: 'Привет!',
+    en: 'Welcome!'
+  },
+  thisIsMyUniWork: {
+    ru: 'Это мой университетский проект',
+    en: 'This is my university project'
+  }
+}
+
+export const useNativeText = () => {
+  const [language, setLanguage] = useState<Language>('ru')
+  const processedTexts: Record<AllTexts, string> =
+    useMemo(() =>
+      Object.fromEntries(Object.entries(texts)
+        .map(([k, v]) => [k, v[language]])) as Record<AllTexts, string>, [language])
+  return {
+    ...processedTexts,
+    language,
+    setLanguage
+  }
+}
