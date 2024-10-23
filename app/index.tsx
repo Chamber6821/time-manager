897bf6d239a46c2993b0a675a2405b95f15f5908
Chamber6821 <vania.osinin@yandex.ru>
2024-10-23 23:32:01 +0300

    Auto login if user already authorized

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,8 @@
+import { useSession } from "@/hooks/useSession";
 import { Redirect } from "expo-router";
 
 export default function Index() {
+  const { username } = useSession()
+  if (username) return <Redirect href="/(app)/" />
   return <Redirect href="/login" />
 }
