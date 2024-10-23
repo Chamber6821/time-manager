dba97a87fc13767e67b81d7f70b4bf5a16dfed9d
Chamber6821 <vania.osinin@yandex.ru>
2024-10-23 03:59:37 +0300

    Add database

---
+++ hooks/useDatabase.ts
@@ -0,0 +1,30 @@
+import { create } from "zustand";
+import { SQLiteDatabase, openDatabaseAsync } from "expo-sqlite";
+
+type Database = {
+  db?: SQLiteDatabase
+}
+
+export namespace Schema {
+  export type user = {
+    id: number,
+    name: string,
+    password: string
+  }
+}
+
+export const useDatabase = create<Database>(() => ({}))
+
+openDatabaseAsync('time')
+  .then(async db => {
+    await db.runAsync(`
+CREATE DATABASE IF NOT EXISTS
+  user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+  )
+`);
+    useDatabase.setState({ db })
+  })
+
---
+++ hooks/useUsers.ts
@@ -0,0 +1,28 @@
+import { Schema, useDatabase } from "./useDatabase"
+
+export type Users = {
+  registered: (name: string) => Promise<boolean>
+  register: (name: string, password: string) => Promise<void>,
+  login: (name: string, password: string) => Promise<boolean>
+}
+
+export const useUsers = (): Users => {
+  const { db } = useDatabase()
+  const databaseNotInitialized = async () => {
+    throw new Error('The database has not been initialized yet, please try again later.')
+  }
+  if (!db) return {
+    registered: databaseNotInitialized,
+    register: databaseNotInitialized,
+    login: databaseNotInitialized
+  }
+  return {
+    registered: async (name: string) =>
+      !!await db.getFirstAsync<Schema.user>('SELECT * FROM user WHERE name = ?', [name]),
+    register: async (name: string, password: string) => {
+      await db.runAsync('INSERT INTO user(name, password) VALUES (?, ?)', [name, password])
+    },
+    login: async (name: string, password: string) =>
+      !!await db.getFirstAsync<Schema.user>('SELECT * FROM user WHERE name = ? AND password = ?', [name, password]),
+  }
+}
