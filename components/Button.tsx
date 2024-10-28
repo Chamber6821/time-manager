e94343d4bb8c3fe51f5fbb0abb4659e730754bd9
Chamber6821 <vania.osinin@yandex.ru>
2024-10-28 23:02:13 +0300

    Maximize button width

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,6 +6,7 @@ export default function Button(props: PressableProps) {
   return (
     <Pressable
       style={{
+        width: '100%',
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: colors.primary,
