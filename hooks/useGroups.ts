eaa27a382be49751bc37afd76d7dc4b2a1779bbf
Chamber6821 <vania.osinin@yandex.ru>
2024-11-22 02:59:04 +0300

    Add useGroups

---
+++ hooks/useGroups.ts
@@ -0,0 +1,76 @@
+import { Schema, useDatabase } from "./useDatabase"
+import { useEffect, useState } from "react"
+
+export type GroupId = number
+
+export type Group = {
+  id: GroupId
+  name: string
+  color: string
+}
+
+export type Groups = {
+  groups: Group[]
+  create: (group: Omit<Group, 'id'>) => Promise<GroupId>
+  update: (id: GroupId, group: Partial<Omit<Group, 'id'>>) => Promise<void>
+  delete: (id: GroupId) => Promise<void>
+  withId: (id: GroupId) => Group
+}
+
+export const useGroups = (): Groups => {
+  const { db } = useDatabase()
+  const [groups, setGroups] = useState<Group[]>([])
+
+  useEffect(() => {
+    if (!db) return
+    (async () => {
+      const grp = await db.getAllAsync<Schema.activityGroup>('SELECT * FROM activity_group')
+      setGroups(
+        grp.map(x => ({
+          id: x.id,
+          name: x.name,
+          color: x.color
+        })).sort((a, b) => a.name.localeCompare(b.name))
+      )
+    })()
+  }, [db])
+
+  if (!db) return {
+    groups: [],
+    create: () => { throw new Error('Database not initialized') },
+    update: () => { throw new Error('Database not initialized') },
+    delete: () => { throw new Error('Database not initialized') },
+    withId: () => { throw new Error('Database not initialized') }
+  }
+
+  return {
+    groups,
+    create: async (group: Omit<Group, 'id'>) => await db.runAsync(
+      'INSERT INTO activity_group(name, color) VALUES(?, ?)',
+      [group.name, group.color]
+    ).then(x => x.lastInsertRowId),
+    update: async (id: GroupId, group: Partial<Omit<Group, 'id'>>) => {
+      await db.runAsync(`
+UPDATE activity_group
+SET
+  name = COALESCE(?, name),
+  color = COALESCE(?, color)
+WHERE id = ?;`,
+        [
+          group.name || null,
+          group.color || null,
+          id
+        ])
+    },
+    delete: async (id: GroupId) => {
+      await db.runAsync('DELETE FROM activity_group WHERE id = ?', [id])
+    },
+    withId: (id: GroupId) => {
+      const group = groups.find(g => g.id === id)
+      if (!group) {
+        return groups[0]
+      }
+      return group
+    }
+  }
+}
