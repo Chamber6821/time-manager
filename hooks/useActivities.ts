5a1f443337429440ec43ac5ca55d0115ca2258e6
Chamber6821 <vania.osinin@yandex.ru>
2024-11-22 02:58:49 +0300

    Simplify useActivities

--- hooks/useActivities.ts
+++ hooks/useActivities.ts
@@ -1,22 +1,25 @@
 import Activity from "@/components/Activity"
 import { Schema, useDatabase } from "./useDatabase"
 import { useEffect, useState } from "react"
+import { GroupId } from "./useGroups"
+
+export type ActivityId = number
 
 export type Activity = {
-  id: number,
-  description: string,
-  started: Date,
-  ended: Date,
-  group: {
-    id: number,
-    name: string,
-    color: string
-  }
+  id: ActivityId
+  description: string
+  started: Date
+  ended: Date
+  groupId: GroupId
 }
+
 export type Activities = {
-  activities: Activity[],
-  create: (description: string, groupId: number, start: Date, end: Date) => Promise<void>
+  activities: Activity[]
+  create: (activity: Omit<Activity, 'id'>) => Promise<ActivityId>
+  update: (id: ActivityId, activity: Partial<Omit<Activity, 'id'>>) => Promise<void>
+  delete: (id: ActivityId) => Promise<void>
 }
+
 export const useActivities = (username: string): Activities => {
   const { db } = useDatabase()
   const [userId, setUserId] = useState(0)
@@ -33,14 +36,13 @@ export const useActivities = (username: string): Activities => {
     if (!userId) return
     (async () => {
       const acts = await db.getAllAsync<Schema.activity>('SELECT * FROM activity WHERE user_id = ?', [userId])
-      const groups = await db.getAllAsync<Schema.activityGroup>('SELECT * FROM activity_group')
       setActivities(
         acts.map(x => ({
           id: x.id,
           description: x.description,
           started: new Date(x.start_time),
           ended: new Date(x.end_time),
-          group: groups.filter(y => y.id === x.group_id)[0]
+          groupId: x.group_id
         })).sort((a, b) => +a.started - +b.started)
       )
     })()
@@ -48,15 +50,35 @@ export const useActivities = (username: string): Activities => {
 
   if (!db || !userId) return {
     activities: [],
-    create: () => { throw new Error('Database not initialized') }
+    create: () => { throw new Error('Database not initialized') },
+    update: () => { throw new Error('Database not initialized') },
+    delete: () => { throw new Error('Database not initialized') }
   }
   return {
     activities,
-    create: async (description: string, groupId: number, start: Date, end: Date) => {
-      await db.runAsync(
-        'INSERT INTO activity(user_id, group_id, start_time, end_time, description) VALUES(?, ?, ?, ?, ?)',
-        [userId, groupId, +start, +end, description]
-      )
+    create: async (activity: Omit<Activity, 'id'>) => await db.runAsync(
+      'INSERT INTO activity(user_id, group_id, start_time, end_time, description) VALUES(?, ?, ?, ?, ?)',
+      [userId, activity.groupId, +activity.started, +activity.ended, activity.description]
+    ).then(x => x.lastInsertRowId),
+    update: async (id: ActivityId, activity: Partial<Omit<Activity, 'id'>>) => {
+      await db.runAsync(`
+UPDATE activity
+SET
+  group_id = COALESCE(?, group_id),
+  start_time = COALESCE(?, start_time),
+  end_time = COALESCE(?, end_time),
+  description = COALESCE(?, description)
+WHERE id = ?;`,
+        [
+          activity.groupId || null,
+          activity.started ? +activity.started : null,
+          activity.ended ? +activity.ended : null,
+          activity.description || null,
+          id
+        ])
+    },
+    delete: async (id: ActivityId) => {
+      await db.runAsync('DELETE FROM activity WHERE id = ?', [id])
     }
   }
 }
